424c90a55a2b107fbe9eff04ee63615bf572ab97
Raneem Qasim <108181104+RaneemQasim5251@users.noreply.github.com>
2023-06-10 06:42:56 +0300

    Create IncDec.php

---
+++ Increment/IncDec.php
@@ -0,0 +1,17 @@
+<?php
+// Increment/decrement operators
+
+    $counter = 0;
+
+   // we can shortcut this Increment: 
+   //$counter = $counter + 1;
+   //to this:
+   //$counter ++;
+   //the same for the decrement:
+   //$counter --;
+   // we can also add or suptract nembers like this:
+   //$counter +=  2;
+   //$counter -= 2;
+
+    echo $counter; 
+?>
