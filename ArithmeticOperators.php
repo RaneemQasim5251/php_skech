d1590d72e403318695e8b3a507b31cfd00accd6f
Raneem Qasim <108181104+RaneemQasim5251@users.noreply.github.com>
2023-06-10 06:26:24 +0300

    Create ArithmeticOperators.php

---
+++ ArithmeticOperators.php
@@ -0,0 +1,16 @@
+<?php
+// Arithmetic operators
+   $x = 10;
+   $y = 3;
+   $z = null;
+
+//add $z = $x + $y;
+//subtract $z = $x - $y;
+//multiplaction $z = $x * $y;
+//division $z = $x / $y;
+//power $z = $x ** $y;
+//modules (helpful for programs that guess if the number is even or odd) $z = $x % $y;
+
+   
+   echo $z;
+?>
